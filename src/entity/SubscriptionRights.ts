109597a2d48a5567241be3715807b90f83503c17
Creanics <csiaud83@gmail.com>
2023-12-27 19:40:12 +0100

    add subscription rights

---
+++ src/entity/SubscriptionRights.ts
@@ -0,0 +1,19 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
+
+@Entity()
+export class SubscriptionRights {
+
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column()
+    name: string
+    @Column()
+    slug: string
+    @Column()
+    description: string
+
+    @Column()
+    active: number
+
+}
